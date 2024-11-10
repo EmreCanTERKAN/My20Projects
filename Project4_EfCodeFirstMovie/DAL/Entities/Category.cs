21f9586f2c2ff1ec7c86801ca28d3d838025b21d
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-11-10 19:53:39 +0300

    Yeni proje ve codefirst yaklaşımı ile veritabanı ayarların yapılandırılması

---
+++ Project4_EfCodeFirstMovie/DAL/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using Project4_EfCodeFirstMovie.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4_EfCodeFirstMovie.DAL.Context
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<Movie> Movies { get; set; }
+
+
+    }
+}
